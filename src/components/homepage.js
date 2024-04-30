633abac4e8aefe8925cbba891f9188b222960170
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-04-30 22:19:41 +0530

    added functions for task extraction

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,16 +1,36 @@
+import { useState , useEffect } from 'react';
 import '../style/home.css'
 
 const Home = () => {
+    const [task,setTask] = useState('')
+    const [taskArray,setTaskArray] = useState([])
+    const handleChange = (event)=>{
+        setTask(event.target.value)
+    }
+    const addTask = ()=>{
+        if(task.length!=0){
+            setTaskArray([...taskArray, task])
+            setTask('')
+        }
+    }
+    useEffect(()=>{
+        console.log(taskArray);
+    },[taskArray])
     return ( 
         <div className="homePage">
             <div className="taskContainer">
                 <div className="title">TodoWebsite</div>
                 <div className="taskInput">
-                    <input type="text" placeholder='Enter the Task here' />
+                    <input className='inputText' value={task} type="text" onChange={handleChange} placeholder='Enter the Task here' />
                 </div>
                 <div className="inputButton">
-                    <input type="button" value="Add Task"/>
+                    <input type="button" value="Add Task" onClick={
+                        ()=>{
+                            addTask()
+                        }
+                    }/>
                 </div>
+
             </div>
         </div>
      );
